88d0d75a7ecbeac69f663652fef05e9f037ce067
Axel Stahl <axelkeizo@gmail.com>
2023-03-05 12:14:25 -0500

    client code

--- client/client.cpp
+++ client/client.cpp
@@ -1,57 +1,82 @@
+#include <iostream>
+#include <string>
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h> 
-
-void error(const char *msg)
-{
-    perror(msg);
-    exit(0);
-}
-
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/uio.h>
+#include <sys/time.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <fstream>
+using namespace std;
+//Client side
 int main(int argc, char *argv[])
 {
-    int sockfd, portno, n; //socket file desciptor, port number, message
-    struct sockaddr_in serv_addr;//server address
-    struct hostent *server;//server/host information
-
-    char buffer[256];//buffer for code
-    if (argc < 3) {
-        fprintf(stderr, "usage %s hostname port\n", argv[0]);
-        exit(0);
+    //we need 2 things: ip address and port number, in that order
+    if(argc != 3)
+    {
+        cerr << "Usage: ip_address port" << endl; exit(0); 
+    } //grab the IP address and port number 
+    char *serverIp = argv[1]; int port = atoi(argv[2]); 
+    //create a message buffer 
+    char msg[1500]; 
+    //setup a socket and connection tools 
+    struct hostent* host = gethostbyname(serverIp); 
+    sockaddr_in sendSockAddr;   
+    bzero((char*)&sendSockAddr, sizeof(sendSockAddr)); 
+    sendSockAddr.sin_family = AF_INET; 
+    sendSockAddr.sin_addr.s_addr = 
+        inet_addr(inet_ntoa(*(struct in_addr*)*host->h_addr_list));
+    sendSockAddr.sin_port = htons(port);
+    int clientSd = socket(AF_INET, SOCK_STREAM, 0);
+    //try to connect...
+    int status = connect(clientSd,
+                         (sockaddr*) &sendSockAddr, sizeof(sendSockAddr));
+    if(status < 0)
+    {
+        cout<<"Error connecting to socket!"<<endl; 
+        exit(1);
     }
-    portno = atoi(argv[2]);//portnumber string into int
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);//init socket
-    if (sockfd < 0) 
-        error("ERROR opening socket");
-    server = gethostbyname(argv[1]);//gethost by the name
-    if (server == NULL) {//gethostbyname() returns null if it doesnt work
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
+    cout << "Connected to the server!" << endl;
+    int bytesRead, bytesWritten = 0;
+    struct timeval start1, end1;
+    gettimeofday(&start1, NULL);
+    while(1)
+    {
+        cout << ">";
+        string data;
+        getline(cin, data);
+        memset(&msg, 0, sizeof(msg));//clear the buffer
+        strcpy(msg, data.c_str());
+        if(data == "exit")
+        {
+            send(clientSd, (char*)&msg, strlen(msg), 0);
+            break;
+        }
+        bytesWritten += send(clientSd, (char*)&msg, strlen(msg), 0);
+        cout << "Awaiting server response..." << endl;
+        memset(&msg, 0, sizeof(msg));//clear the buffer
+        bytesRead += recv(clientSd, (char*)&msg, sizeof(msg), 0);
+        if(!strcmp(msg, "exit"))
+        {
+            cout << "Server has quit the session" << endl;
+            break;
+        }
+        cout << "Server: " << msg << endl;
     }
-    bzero((char *) &serv_addr, sizeof(serv_addr));//turn serv_addr into zeros
-    serv_addr.sin_family = AF_INET;//message over internet type
-    bcopy((char *)server->h_addr, 
-        (char *)&serv_addr.sin_addr.s_addr,
-        server->h_length);//bzero for h_addr, its a string
-    serv_addr.sin_port = htons(portno);//network addr portnumber
-    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) //
-        error("ERROR connecting");
-    printf("Please enter the message: ");
-    bzero(buffer,256);
-    fgets(buffer,255,stdin);//get message
-    n = write(sockfd,buffer,strlen(buffer));//write message to the socket
-    if (n < 0) 
-        error("ERROR writing to socket");
-    bzero(buffer,256);
-    n = read(sockfd,buffer,255);//read from socket
-    if (n < 0) 
-        error("ERROR reading from socket");
-    printf("%s\n",buffer);
-    close(sockfd);//close socket
-    return 0;
+    gettimeofday(&end1, NULL);
+    close(clientSd);
+    cout << "********Session********" << endl;
+    cout << "Bytes written: " << bytesWritten << 
+    " Bytes read: " << bytesRead << endl;
+    cout << "Elapsed time: " << (end1.tv_sec- start1.tv_sec) 
+      << " secs" << endl;
+    cout << "Connection closed" << endl;
+    return 0;    
 }
